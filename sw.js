5b9ba73021182cb9ea5e4295640bb2e9b2f78d4e
Victor Soto <vicsotojr@gmail.com>
2025-02-15 09:58:47 -0500

    Season 2 Initial Update!

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'cbt-league-v1';
+const urlsToCache = [
+  '/index.html',
+  '/manifest.json',
+  '/assets/img/cbt-logo1.jpg',
+  '/assets/css/style.css',  // Add your actual CSS files
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch resources from cache or network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate service worker and remove old cache versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
